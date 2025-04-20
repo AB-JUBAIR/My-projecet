e4bac11cb3e4319becdada80aa177e91b074158b
ab-jubair <jubair7624@gmail.com>
2025-04-20 13:13:37 +0600

    add javascript

--- practice js/array.js
+++ practice js/array.js
@@ -1,2 +1,14 @@
 const numbers = [12, 251,2442, 45];
 const frieds = ['abul', 'babul', 'cabul', 'kabu'];
+
+/**
+ * array length const A =[0, 3,3 , 5, 5, 7 ,5 ,4 ];
+ * console.log(A.length);
+ *
+ */
+
+for (let i = 0; i < numbers.length; i++) 
+    {
+    const element = numbers[i];
+    console.log(element);
+}
