6dff3ed8a89b2527f663b34e272473a578d2893c
ab-jubair <jubair7624@gmail.com>
2025-04-20 14:15:26 +0600

    add javascript

--- practice js/recapAndLopp.js
+++ practice js/recapAndLopp.js
@@ -24,4 +24,15 @@ num++ // num= num +1; /// num += 1;
  console.log(num);
 
 }
+console.log('-----------------------');
 
+// even number jor sonkka
+for (let index = 0; index <=20; index= index +2) {
+    console.log(index);
+}
+// odd number  vijor sonkka
+console.log('------------------');
+
+for (let index = 1; index <=20; index= index +2) {
+    console.log(index);
+}
