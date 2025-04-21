8a65e9365807dc8974afe96c6d37378e5c67c29d
ab-jubair <jubair7624@gmail.com>
2025-04-21 21:49:33 +0600

    add javascriopt

---
+++ practice js/sliice.js
@@ -0,0 +1,32 @@
+const address = 'andarkillah'
+
+const part = address.slice(2, 5)
+console.log(part);
+
+console.log('-----------------');
+
+const setence = 'i am a good student and hard worker'
+console.log(setence.split());// split er kaj. hocce vagkora
+console.log(setence.split(''));
+console.log(setence.split(' '));
+console.log(setence.split('a'));
+
+const realfriedns = ['rohim', 'korim', 'joshim'];
+
+console.log(realfriedns.join('--'));
+
+const first = 'jubair '
+const last = 'abdulah'
+
+
+const fullname = first+last
+console.log(fullname);
+const fullname2 = first.concat(' ').concat(last); // first naem and last name r maj khane white spacce add kora
+
+console.log(fullname2);
+
+console.log(fullname.includes('jubair'));
+
+
+
+
