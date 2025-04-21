d0e924b6035fd7ad9fe816578d9123efe59cbe4e
ab-jubair <jubair7624@gmail.com>
2025-04-21 22:24:44 +0600

    add javascriopt

---
+++ practice js/object.js
@@ -0,0 +1,28 @@
+const age = 21; 
+ const school = 'mghy schoool';
+ const bottleWrong = ['white', 45];
+
+ /**
+  * how to create object 
+  * Non-Prmitive
+  */
+
+ const bottle = {
+    brand: 'apple',
+    price: 25,
+    color: 'white',
+    isClean: false
+ }
+ const subject =  {
+    name:  'biology',
+    teacher: 'rasheda  maam',
+    exammernar: '30 feb',
+    chapter: ['first', 'second']
+ }
+//  console.log(subject);
+ 
+ console.log(subject.name); // dot notation 
+ // bracker notation 
+
+ console.log(subject['chapter']);
+ 
