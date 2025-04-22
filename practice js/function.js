17be7d5c279037e38fb8e7d80213c9508d119e82
ab-jubair <jubair7624@gmail.com>
2025-04-22 20:29:31 +0600

    add javascriopt

---
+++ practice js/function.js
@@ -0,0 +1,27 @@
+//  
+
+function fanoffkor ()  //function decleration 
+{
+     console.log('i am a student');
+
+     
+}
+fanoffkor(); // function call kora
+
+const n = 5;
+
+function square (n)
+{
+    let total = n*n;
+    console.log(total);
+    
+}
+square(5);
+
+function add (num1, num2)
+{
+    const sum = num1 +num2;
+    console.log('total number ',sum);
+    
+}
+add(45, 50)
