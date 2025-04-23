933d2af390ae94cceb77bbc0c33805eade846aec
ab-jubair <jubair7624@gmail.com>
2025-04-23 21:58:43 +0600

    add javascriopt

--- practice js/ArrayTask.js
+++ practice js/ArrayTask.js
@@ -1,12 +1,12 @@
-// const Fruits =['mango', 'grave', 'pinaple', 'suggarcan', 'apple'];
-// Fruits[2] = 'jambura'
-// console.log('3rd index Number will be',Fruits[3]);
+const Fruits =['mango', 'grave', 'pinaple', 'suggarcan', 'apple'];
+Fruits[2] = 'jambura'
+console.log('3rd index Number will be',Fruits[3]);
 
-// for (const fruit of Fruits) {
-//     console.log(fruit);
+for (const fruit of Fruits) {
+    console.log(fruit);
     
-// }
-
+}
+//----------------------
 const books = [];
 books.push('cox bazar');
 books.push('bandarban', 'andarban');
