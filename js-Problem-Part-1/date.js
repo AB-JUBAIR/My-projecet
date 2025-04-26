4a35a22221908c488a93a5df448c80ca238af275
ab-jubair <jubair7624@gmail.com>
2025-04-26 22:38:57 +0600

    get element by tag naem

---
+++ js-Problem-Part-1/date.js
@@ -0,0 +1,10 @@
+/**
+ * date function 
+ */
+
+const today = new Date();
+const date = new Date('2010-10-19')
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getFullYear());
---
+++ js-Problem-Part-1/math-min-max.js
@@ -0,0 +1,14 @@
+const min = Math.min(12,425,15,15124,215,21,451);
+console.log(min);
+
+const max = Math.max(12,58,12,41,2,5,1,4,255)
+console.log(max);
+
+const abs = Math.abs(-25)
+console.log(abs);
+console.log(Math.round(2.4));
+console.log(Math.round(2.6)); // kon float number k intiger number kkora
+console.log(Math.floor(2.6)); // nice namypela
+console.log(Math.ceil(2.6));// upore uthano
+console.log(Math.random()*100);// upore uthano
+console.log(Math.round(Math.random()*100)); 
