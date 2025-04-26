f4c6827867e6202625a7dc77dd94acc3630e46e2
ab-jubair <jubair7624@gmail.com>
2025-04-26 20:04:09 +0600

    get element by tag naem

---
+++ js-Problem/basic.js
@@ -0,0 +1,36 @@
+/**
+ * variable --- 
+ * nummber and string 
+ * boolean 
+non-primitive
+ * array 
+ * boject
+ * 
+ * 
+ * 
+ * condition--------- > < >= <= != == ===
+ * 
+ * if (-------)
+ * 
+ * else 
+ * 
+ * array = []
+ * 
+ * push and pop 
+ * shift and unshipt
+ * slice 
+ * 
+ * loop------- forof and for and forin and while loop
+ * break and continiue 
+ * 
+ * bojecet 
+ * 
+ * function (parameter)
+ * 
+ * {
+ * return
+ * }
+ * 
+ * 
+ * 
+ */
