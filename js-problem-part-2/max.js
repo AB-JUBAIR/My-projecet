c6a087dfa6f84e32d16bb2a3ea54cf265e67f828
ab-jubair <jubair7624@gmail.com>
2025-04-27 15:56:16 +0600

    problem part 2

--- js-problem-part-2/max.js
+++ js-problem-part-2/max.js
@@ -14,3 +14,17 @@ const max2 = getMax (36, 26);
 
 const ultimatemax = getMax (max1, max2);
 console.log(ultimatemax);
+
+// const max = Math.max(12,25521,2245,12,52212,1,512);
+
+
+const height = [65, 55,24,68,20, 250];
+
+let max = 0;
+
+for (const item of height) {
+    if (max < item ) {
+        max = item;
+    }
+}
+console.log('the max in this array',max);
