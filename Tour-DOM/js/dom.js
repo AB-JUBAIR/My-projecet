0688535c434688a7f5bd36ada26568796867e7f4
ab-jubair <jubair7624@gmail.com>
2025-04-28 13:42:39 +0600

    add element

--- Tour-DOM/js/dom.js
+++ Tour-DOM/js/dom.js
@@ -1,3 +1,30 @@
+
+/**
+ * 
+ */
+
+const maintcotainer = document.getElementById('main-content');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'New sectioin'  ;
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+li.innerText = 'biriyani';
+ul.appendChild(li);
+const li1 = document.createElement('li');
+li1.innerText = 'juich';
+ul.appendChild(li1);
+const li2 = document.createElement('li');
+li2.innerText = 'kacchi';
+ul.appendChild(li2);
+section.appendChild(ul);
+maintcotainer.append(section);
+// ---------- style sectijon --------------
+console.log('Js is connected');
+
 const sections  = document.querySelectorAll('section');
 
 for (const section of sections) {
