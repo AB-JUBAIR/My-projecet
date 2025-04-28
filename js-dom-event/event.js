b2684112b9189b333d5928528a21940b95d51583
ab-jubair <jubair7624@gmail.com>
2025-04-28 21:22:42 +0600

    update

--- js-dom-event/event.js
+++ js-dom-event/event.js
@@ -4,3 +4,18 @@ makepurple.onclick = function makepurpleit()
 {
     document.body.style.backgroundColor= 'purple';
 }
+// option 2
+function makeYellow()
+{
+    document.body.style.backgroundColor = 'yellow';
+}
+// option 4
+const Pink = document.getElementById('Pink');
+Pink.addEventListener('click', makepink); 
+
+function makepink() {
+    document.body.style.backgroundColor = 'pink';
+}
+//option 4 final it is the best and cleanest way i will use most of the time
+ document.getElementById('green').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green'}); 
