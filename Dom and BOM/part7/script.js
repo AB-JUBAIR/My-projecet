e6343bab72edc9e2a3851d0ba19bef619c8cdc26
ab-jubair <jubair7624@gmail.com>
2025-06-16 18:23:29 +0600

    update

---
+++ Dom and BOM/part7/script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn2").addEventListener('click', function()  {
+    console.log('this');
+    let hold = document.getElementById("btn");
+    console.log(hold);
+    hold.textContent = 'jubair'
+    
+    
+})
