58679dd1cd8a317bf6a57fd8c599a1a9d51d1f93
ab-jubair <jubair7624@gmail.com>
2025-06-16 17:40:33 +0600

    sample

---
+++ ES6 intro/index.php
@@ -0,0 +1,10 @@
+<?php
+// Display a welcome message
+echo "Welcome to my PHP page!<br>";
+
+// Display the current date and time
+date_default_timezone_set("Asia/Dhaka"); // Set your timezone
+echo "Today's date is: " . date("Y-m-d") . "<br>";
+echo "Current time is: " . date("h:i:s A");
+>
+`
