144df656f79ac9dcade52e609d36cad887f0ee8e
ab-jubair <jubair7624@gmail.com>
2025-06-17 10:50:58 +0600

    sample

--- Dom and BOM/part7/script.js
+++ Dom and BOM/part7/script.js
@@ -3,6 +3,11 @@ document.getElementById("btn2").addEventListener('click', function()  {
     let hold = document.getElementById("btn");
     console.log(hold);
     hold.textContent = 'jubair'
-    
-    
+})
+// add to shoping list 
+document.getElementById("addNewItem").addEventListener('click', function() {
+    let newItem = document.createElement('li')
+    newItem.textContent = "egg"
+
+    document.getElementById("shopingList").appendChild(newItem);
 })
