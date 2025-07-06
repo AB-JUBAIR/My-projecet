583d3b34025a2a58e1edb5f2c5cc937106405a8d
ab-jubair <jubair7624@gmail.com>
2025-07-06 22:40:52 +0600

    js update

---
+++ advanceJS/asynchronous.js
@@ -0,0 +1,22 @@
+/**
+ * having pause |-------> network calls 
+ *              |------->   write / read files 
+ *              |------->   time functiono 
+ *              |------->   userINput
+ *              |------->   Other.....
+ */
+console.log("chaicode");
+
+function SayHello (){
+    console.log("i would like to say hello");
+    
+}
+setTimeout(() => {
+    SayHello()
+}, 4000); // 4 munits
+
+for (let index = 0; index < 10; index++) {
+console.log(index);
+
+
+}
