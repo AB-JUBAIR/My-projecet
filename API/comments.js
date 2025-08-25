349064e8f27f0c121efc149e0d1d3202b8d4ef2b
ab-jubair <jubair7624@gmail.com>
2025-08-26 00:43:26 +0600

    update

--- API/comments.js
+++ API/comments.js
@@ -1,3 +1,4 @@
+// method 1 for API intigration
 const LoadComments = () => {
 
     const url = 'https://jsonplaceholder.typicode.com/comments'
@@ -6,7 +7,7 @@ const LoadComments = () => {
     .then(data => console.log(data))
     .catch(error => console.error('error happened', error))
 }
-
+// method 2
 try {
 const LoadComments2 = async() => {
  const res = await fetch('https://jsonplaceholder.typicode.com/comments')
